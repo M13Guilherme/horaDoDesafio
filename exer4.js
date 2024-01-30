6cd9caf7d38628400b995af989a5887b22a1dcf6
Mike Guilherme <153580557+M13Guilherme@users.noreply.github.com>
2024-01-29 22:48:18 -0300

    Add files via upload

---
+++ exer4.js
@@ -0,0 +1,71 @@
+//exer 1
+console.log('Boas vindas ao exercício de desafio final');
+
+//exer 2
+let nome = Mike;
+console.log(`Olá ${nome}!`);
+
+//exer 3
+let nomee = Mike;
+alert(`Olá ${nomee}`);
+
+//exer 4
+lingProg = prompt('Qual é a linguagem de programação que você mais gosta?');
+console.log (lingProg);
+
+//exer 5
+let Valor1 = 330;
+let Valor2 = 110;
+let resultado = Valor1 + Valor2;
+console.log(`A soma de ${Valor1} e ${Valor2} é ${resultado}.`);
+
+//exer 6
+let Valor3 = 13;
+let Valor4 = 8;
+let resultadoo = Valor3 - Valor4;
+console.log(`A diferença entre ${Valor3} e ${Valor4} é de ${resultadoo}.`); 
+
+//exer 7
+let idade = prompt('Por favor insira a sua idade');
+   if (idade >= 18) {
+    console.log('Você é maior de idade!');
+    } else {
+    console.log('Você é menor de idade');
+   }
+
+//exer 8
+let numero =prompt('digite um número');
+   if (numero > 0) {   
+    alert('O número é positivo');
+}else if (numero < 0) {
+    alert('O número é negativo');
+}else{
+    alert('O número é zero');
+}
+
+//exer 9
+let contagem = 1
+while (contagem <= 10); {
+    console.log(contagem);
+    contagem++
+}
+
+//exer 10
+let nota = 9
+if (nota >= 7) {
+    console.log('Você está APROVADO');  
+}else{
+    console.log('Você está REPROVADO');
+}
+
+//exer 11
+let numeroAleatorio = Math.random();
+console.log(numeroAleatorio);
+
+//exer 12
+let numeroInteiro = parseInt(Math.random() * 10) + 1;
+console.log(numeroInteiro);
+
+//exer 13
+let numeroIntAlea = parseInt(Math.random() * 1000) + 1;
+console.log(numeroIntAlea);
